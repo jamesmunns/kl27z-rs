8171217de8c4be3bc13456695576956c4bd10555
James Munns <james.munns@gmail.com>
2016-10-09 02:06:36 +0200

    Add "exception" examples

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 // those are not available in this platform.
 #![no_std]
 
+#![feature(asm)]
+
 // Conceptually, this is our program "entry point". It's the first thing the microcontroller will
 // execute when it (re)boots. (As far as the linker is concerned the entry point must be named
 // `start` (by default; it can have a different name). That's why this function is `pub`lic, named
@@ -20,6 +22,11 @@ pub fn start() -> ! {
     let x = 42;
     let y = x;
 
+//    unsafe {
+//        let ram_boundary = *(0x0000_0000 as *const u32);
+//        let _crash = *(ram_boundary as *const u32);
+//    }
+
     // We can't return from this function so we just spin endlessly here.
     loop {}
 }
@@ -28,7 +35,34 @@ pub fn start() -> ! {
 mod vector_table {
     #[link_section = ".reset"]
     static RESET: fn() -> ! = ::start;
+
+    #[link_section = ".exceptions"]
+    static EXCEPTIONS: [Option<fn() -> !>; 14] = [Some(::exception::handler),  // NMI
+                                                  Some(::exception::handler),  // Hard fault
+                                                  Some(::exception::handler),  // Memory management fault
+                                                  Some(::exception::handler),  // Bus fault
+                                                  Some(::exception::handler),  // Usage fault
+                                                  None, // Reserved
+                                                  None, // Reserved
+                                                  None, // Reserved
+                                                  None, // Reserved
+                                                  Some(::exception::handler),  // SVCall
+                                                  None, // Reserved for Debug
+                                                  None, // Reserved
+                                                  Some(::exception::handler),  // PendSV
+                                                  Some(::exception::handler)]; // Systick
 }
+
+mod exception {
+    pub fn handler() -> ! {
+        unsafe {
+            asm!("bkpt");
+        }
+
+        loop {}
+    }
+}
+
 
 // Finally, we need to define the panic_fmt and eh_personality "lang item"s, which are just
 // functions. This specifies what the program should do when a `panic!` occurs. Our program won't
